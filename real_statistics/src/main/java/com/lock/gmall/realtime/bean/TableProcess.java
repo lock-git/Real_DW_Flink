ffe9baa2fb0abb4b77228c2162e4976b0db14d77
xiashiyong <xiashiyong@jiangduoduo.com>
2021-03-03 15:57:08 +0800

    补全函数

--- real_statistics/src/main/java/com/lock/gmall/realtime/bean/TableProcess.java
+++ real_statistics/src/main/java/com/lock/gmall/realtime/bean/TableProcess.java
@@ -17,6 +17,63 @@ public class TableProcess {
     String sourceTable;
     //操作类型 insert,update,delete
     String operateType;
+
+    public String getSourceTable() {
+        return sourceTable;
+    }
+
+    public void setSourceTable(String sourceTable) {
+        this.sourceTable = sourceTable;
+    }
+
+    public String getOperateType() {
+        return operateType;
+    }
+
+    public void setOperateType(String operateType) {
+        this.operateType = operateType;
+    }
+
+    public String getSinkType() {
+        return sinkType;
+    }
+
+    public void setSinkType(String sinkType) {
+        this.sinkType = sinkType;
+    }
+
+    public String getSinkTable() {
+        return sinkTable;
+    }
+
+    public void setSinkTable(String sinkTable) {
+        this.sinkTable = sinkTable;
+    }
+
+    public String getSinkColumns() {
+        return sinkColumns;
+    }
+
+    public void setSinkColumns(String sinkColumns) {
+        this.sinkColumns = sinkColumns;
+    }
+
+    public String getSinkPk() {
+        return sinkPk;
+    }
+
+    public void setSinkPk(String sinkPk) {
+        this.sinkPk = sinkPk;
+    }
+
+    public String getSinkExtend() {
+        return sinkExtend;
+    }
+
+    public void setSinkExtend(String sinkExtend) {
+        this.sinkExtend = sinkExtend;
+    }
+
     //输出类型 hbase kafka
     String sinkType;
     //输出表(主题)
